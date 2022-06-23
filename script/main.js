3799a94db667f2080aa4376da9d110af5cb11666
Isaac Evelyn <isaacevelyn93@gmail.com>
2022-06-23 15:53:47 +0100

    developed calculator

--- script/main.js
+++ script/main.js
@@ -1,4 +1,60 @@
 let inputEl = document.querySelector("#input")
-function one(){
-   
+let numDiv = document.getElementById("numbers")
+let signDiv = document.getElementById("signs")
+
+numDiv.addEventListener('click', function (ev) {
+    if (ev.target = "button") {
+        let val = ev.target
+        inputEl.value += val.textContent
+    }
+})
+
+signDiv.addEventListener('click', function (ev) {
+    if (ev.target = "button") {
+        let val = ev.target
+        let str = inputEl.value
+
+        inputEl.value += val.textContent
+
+        // if (ev.path[1].id = "signs") {
+        //     let last = str.charAt(str.length - 1);
+        //     let result = str.replace(last, val.textContent)
+        //     console.log(result)
+        //     inputEl.value = result
+        // }
+
+    }
+})
+// calculate.addEventListener('click', function (ev) {
+//     if (ev.target = "button") {
+
+//         let val = ev.target
+//         console.log(val)
+
+
+//         if (ev.path[0].innerText = "del") {
+//         }
+//         else if (val.textContent = "clear") {
+           
+        // }
+
+//     }
+// })
+
+function del(){
+    inputEl.value = inputEl.value.slice(0, -1)
+}
+function clearInput(){
+    if (inputEl.value = ""){
+        return;
+    } else {
+        inputEl.val = ""
+    }
+}
+function calculate(){
+    try {
+       inputEl.value = eval(inputEl.value)  
+    } catch (err) {
+     inputEl.value = "error"   
+    }
 }
